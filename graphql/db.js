cccd29cccb132121eca02a46b941852e40e45880
Jinchul park <jinchul112@gmail.com>
2020-04-14 15:14:13 +0200

    second commit

--- graphql/db.js
+++ graphql/db.js
@@ -1,25 +1,50 @@
-export const people =[
+let movies =[
+    {
+        id: 0,
+        name: "Oldboy",
+        score: 1
+    },
     {
         id: 1,
-        name: "Jin",
-        age: 29,
-        gender: "Male"
+        name: "Parasite",
+        score: 1
     },
     {
         id: 2,
-        name: "rt",
-        age: 11,
-        gender: "Female"
+        name: "StarWars",
+        score: 2
     },
     {
         id: 3,
-        name: "park",
-        age: 21,
-        gender: "Male"
+        name: "Harry Potter",
+        score: 3
     }
 ]
 
+export const getMovie = () => movies
+
 export const getById = id => {
-    const filtered = people.filter(person => person.id === id);
+    const filtered = movies.filter(movie => movie.id === id);
     return filtered[0];
+}
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if (movies.length > cleanedMovies.length){
+        movies = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+};
+
+export const addMovie = (name, score) => {
+    const newMovie ={
+        id : `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    console.log(movies);
+    return newMovie;
 }
